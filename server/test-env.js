753cfdf6e38467b4893097787af470a6b2084fce
rohitkr5850 <rroy854100@gmail.com>
2025-06-29 07:44:49 +0530

    Update

--- server/test-env.js
+++
@@ -1,5 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
-console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
